77165fa2a45773d9fda1574d07bf2f6e92511383
Kieran Bingham <kieran.bingham@linaro.org>
2016-01-22 17:09:39 +0000

    scripts/gdb: Provide auto loading class
    When the GDB Scripts are loaded, we will want to take the opportunity to
    detect our underlying symbols, and if possible register some early hooks
    
    Signed-off-by: Kieran Bingham <kieran.bingham@linaro.org>

--- scripts/gdb/linux/__init__.py
+++ scripts/gdb/linux/__init__.py
@@ -1,1 +1,13 @@
-# nothing to do for the initialization of this package
+# Initialisation of Kernel Awareness in GDB.
+
+import gdb
+
+
+class LxAutostart():
+    def __init__(self):
+        gdb.write("Linux Kernel Debugger Loading\n")
+        gdb.write("Detected the following kernel vesion\n\n")
+        gdb.write(gdb.parse_and_eval("linux_banner").string())
+        gdb.write("\n\n")
+
+LxAutostart()
