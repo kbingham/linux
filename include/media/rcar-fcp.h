0df11ed58870eec54cbf53e07eb00c01ac9cc95e
Kieran Bingham <kieran@bingham.xyz>
2016-05-25 13:32:56 +0100

    fcp: Provide mapped register accessor
    This is really just for debug, so that I can view the status
    registers on the FCPF

--- include/media/rcar-fcp.h
+++ include/media/rcar-fcp.h
@@ -16,11 +16,17 @@
 struct device_node;
 struct rcar_fcp_device;
 
+#define FCP_VCR		0x0000
+#define FCP_RST		0x0010
+#define FCP_STA		0x0018
+
 #if IS_ENABLED(CONFIG_VIDEO_RENESAS_FCP)
 struct rcar_fcp_device *rcar_fcp_get(const struct device_node *np);
 void rcar_fcp_put(struct rcar_fcp_device *fcp);
 int rcar_fcp_enable(struct rcar_fcp_device *fcp);
 void rcar_fcp_disable(struct rcar_fcp_device *fcp);
+void __iomem * rcar_fcp_regs(struct rcar_fcp_device *fcp);
+u32 rcar_fcp_read(struct rcar_fcp_device *fcp, unsigned int reg);
 #else
 static inline struct rcar_fcp_device *rcar_fcp_get(const struct device_node *np)
 {
@@ -32,6 +38,14 @@ static inline int rcar_fcp_enable(struct rcar_fcp_device *fcp)
 	return -ENOSYS;
 }
 static inline void rcar_fcp_disable(struct rcar_fcp_device *fcp) { }
+static inline void __iomem * rcar_fcp_regs(struct rcar_fcp_device *fcp)
+{
+	return NULL;
+}
+static u32 rcar_fcp_read(struct rcar_fcp_device *fcp, unsigned int reg)
+{
+	return 0;
+}
 #endif
 
 #endif /* __MEDIA_RCAR_FCP_H__ */
