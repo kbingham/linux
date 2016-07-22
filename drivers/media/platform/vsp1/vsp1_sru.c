f37d28be4fd521e199c3d8d8335ec1d9363695ad
Kieran Bingham <kieran@bingham.xyz>
2016-07-22 15:30:32 +0100

    v4l: vsp1: Define SRU maximum widths for partition algorithm

--- drivers/media/platform/vsp1/vsp1_sru.c
+++ drivers/media/platform/vsp1/vsp1_sru.c
@@ -305,8 +305,28 @@ static void sru_configure(struct vsp1_entity *entity,
 	vsp1_sru_write(sru, dl, VI6_SRU_CTRL2, param->ctrl2);
 }
 
+static unsigned int sru_max_width(struct vsp1_entity *entity,
+				  struct vsp1_pipeline *pipe)
+{
+	struct vsp1_sru *sru = to_sru(&entity->subdev);
+	struct v4l2_mbus_framefmt *input;
+	struct v4l2_mbus_framefmt *output;
+
+	input = vsp1_entity_get_pad_format(&sru->entity, sru->entity.config,
+					   SRU_PAD_SINK);
+	output = vsp1_entity_get_pad_format(&sru->entity, sru->entity.config,
+					    SRU_PAD_SOURCE);
+
+	if (input->width != output->width)
+		return 512;
+
+	/* No limit imposed */
+	return SRU_MAX_SIZE;
+}
+
 static const struct vsp1_entity_operations sru_entity_ops = {
 	.configure = sru_configure,
+	.max_width = sru_max_width,
 };
 
 /* -----------------------------------------------------------------------------
